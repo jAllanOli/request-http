c53ab55e46423ca797d5d56adedb562e81c1054f
allan <j.a.o.huthan@gmail.com>
2023-05-12 17:00:24 -0300

    upload component

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -6,12 +6,22 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./upload-file.component.less"],
 })
 export class UploadFileComponent implements OnInit {
+  files: Set<File>;
+
   constructor() {}
 
   ngOnInit() {}
 
   onChange(event) {
     console.log(event);
+    this.files = new Set();
     const selectedFiles = <FileList>event.srcElement.files;
+    for (let i = 0; i < selectedFiles.length; i++) {
+      this.files.add(selectedFiles[i]);
+    }
+  }
+
+  onUpload() {
+    //if(this)
   }
 }
