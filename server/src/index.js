c25657bae499bfed799b26918d60669613887268
allan <j.a.o.huthan@gmail.com>
2023-05-15 13:23:10 -0300

    file upload and download implemented

--- server/src/index.js
+++ server/src/index.js
@@ -7,21 +7,29 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: "*",
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "*",
+//   optionSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions));
 
 const multipartMiddleware = multipart({ uploadDir: "./uploads" });
-app.post("./upload", multipartMiddleware, (req, res) => {
+app.post("/upload", multipartMiddleware, (req, res) => {
   const files = req.files;
   console.log(files);
   res.json({ message: files });
 });
 
+app.get("/downloadExcel", (req, res) => {
+  res.download("./uploads/planilha.xlsx");
+});
+
+app.get("/downloadPDF", (req, res) => {
+  res.download("./uploads/doc.pdf");
+});
+
 app.use((err, req, res, next) => res.json({ error: err.message }));
 
-app.listen(8000, () => {
-  console.log("rodou");
+app.listen(8000, (port) => {
+  console.log("rodando");
 });
--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 
+import { UploadFileService } from "../upload-file.service";
+import { environment } from "src/environments/environment";
+import { filterResponse, uploadProgress } from "src/app/shared/rxjs-operators";
+
 @Component({
   selector: "app-upload-file",
   templateUrl: "./upload-file.component.html",
@@ -7,8 +11,9 @@ import { Component, OnInit } from "@angular/core";
 })
 export class UploadFileComponent implements OnInit {
   files: Set<File>;
+  progress = 0;
 
-  constructor() {}
+  constructor(private service: UploadFileService) {}
 
   ngOnInit() {}
 
@@ -19,9 +24,47 @@ export class UploadFileComponent implements OnInit {
     for (let i = 0; i < selectedFiles.length; i++) {
       this.files.add(selectedFiles[i]);
     }
+    this.progress = 0;
   }
 
   onUpload() {
-    //if(this)
+    if (this.files && this.files.size > 0) {
+      this.service
+        .upload(this.files, `${environment.BASE_URL}/upload`)
+        .pipe(
+          uploadProgress((progress) => {
+            console.log(progress);
+            this.progress = progress;
+          }),
+          filterResponse()
+        )
+        .subscribe((response) => console.log("upload concluido"));
+      /* .subscribe((event: HttpEvent<Object>) => {
+          //HttpEventType
+          console.log(event);
+          if (event.type === HttpEventType.Response) {
+            console.log("Upload Concluído");
+          } else if (event.type === HttpEventType.UploadProgress) {
+            const percentDone = Math.round((event.loaded * 100) / event.total);
+            console.log("Progresso:", percentDone);
+            this.progress = percentDone;
+          }
+        }); */
+    }
+  }
+
+  onDownloadPDF() {
+    this.service
+      .download(`${environment.BASE_URL}/downloadPDF`)
+      .subscribe((res: any) => {
+        this.service.handleFile(res, "doc.pdf");
+      });
+  }
+  onDownloadExcel() {
+    this.service
+      .download(`${environment.BASE_URL}/downloadExcel`)
+      .subscribe((res: any) => {
+        this.service.handleFile(res, "planilhinha.xlsx");
+      });
   }
 }
